f893f1e4d84ea5b172fa0680e80830315e9568df
amirrezaask <raskarpour@gmail.com>
2019-08-24 22:46:12 +0430

    some cleaning

--- fact.go
+++ fact.go
@@ -7,12 +7,8 @@ import (
 	"time"
 )
 
-var buff chan *big.Int
-var wg sync.WaitGroup
-
-func supervisor(i, until, done int64) {
+func supervisor(buff chan *big.Int, wg *sync.WaitGroup, i, until int64) {
 	f := fact(big.NewInt(i), big.NewInt(until))
-	// log.Printf("Fact of %v till %v is %v\n", until, i, f)
 
 	buff <- f
 
@@ -34,20 +30,18 @@ func fact(i, until *big.Int) *big.Int {
 //Fact calculates Fact
 func Fact(target int, ngr int) *big.Int {
 
-	buff = make(chan *big.Int, ngr)
+	buff := make(chan *big.Int, ngr)
 	result := make(chan *big.Int)
+	wg := &sync.WaitGroup{}
 
 	for i := 0; i < ngr; i++ {
-		// log.Printf("UP:%v DOWN:%v", (target/f.NumberOfGoroutines)*(i+1), (target/f.NumberOfGoroutines)*i)
 		func(i int) {
-			// log.Printf("Goroutine #%v", i)
 			wg.Add(1)
-			go supervisor(int64((target/ngr)*(i+1)), int64((target/ngr)*i), int64(target))
+			go supervisor(buff, wg, int64((target/ngr)*(i+1)), int64((target/ngr)*i))
 		}(i)
 	}
 	go func() {
 
-		// log.Println("waiting for facts to be ready")
 		wg.Wait()
 		close(buff)
 		var val = big.NewInt(1)
@@ -56,7 +50,6 @@ func Fact(target int, ngr int) *big.Int {
 
 			temp := big.NewInt(1)
 			val = temp.Mul(val, b)
-			// fmt.Printf("val %v\n", val)
 		}
 		result <- val
 	}()
@@ -65,6 +58,6 @@ func Fact(target int, ngr int) *big.Int {
 
 func main() {
 	s := time.Now()
-	_ = Fact(5, 5)
+	_ = Fact(1000000, 5000)
 	fmt.Println(time.Since(s))
 }
